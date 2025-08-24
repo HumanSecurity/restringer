be7390e65ba47a30f951467b696a9be1ab0b29b3
Ben Baryo <19845603+ctrl-escp@users.noreply.github.com>
2025-08-24 17:54:32 +0300

    Refactor normalization logic in normalizeComputed.js to enhance clarity and functionality. Split the normalization process into separate functions for matching and transforming computed properties, improving code organization and readability. Update comments for better understanding of the transformation process.

--- src/modules/safe/normalizeComputed.js
+++ src/modules/safe/normalizeComputed.js
@@ -1,25 +1,26 @@
 import {badIdentifierCharsRegex, validIdentifierBeginning} from '../config.js';
 
 /**
- * Change all member expressions and class methods which has a property which can support it - to non-computed.
- * E.g.
- *   console['log'] -> console.log
- * @param {Arborist} arb
+ * Find all computed member expressions, method definitions, and properties that can be converted to dot notation.
+ * @param {Arborist} arb An Arborist instance
  * @param {Function} candidateFilter (optional) a filter to apply on the candidates list
- * @return {Arborist}
+ * @return {Array} Array of nodes that match the criteria for normalization
  */
-function normalizeComputed(arb, candidateFilter = () => true) {
-	const relevantNodes = [
-		...(arb.ast[0].typeMap.MemberExpression || []),
-		...(arb.ast[0].typeMap.MethodDefinition || []),
-		...(arb.ast[0].typeMap.Property || []),
-	];
+export function normalizeComputedMatch(arb, candidateFilter = () => true) {
+	const relevantNodes = []
+		.concat(arb.ast[0].typeMap.MemberExpression)
+		.concat(arb.ast[0].typeMap.MethodDefinition)
+		.concat(arb.ast[0].typeMap.Property);
+
+	const matchingNodes = [];
+	const relevantTypes = ['MethodDefinition', 'Property'];
+	
 	for (let i = 0; i < relevantNodes.length; i++) {
 		const n = relevantNodes[i];
-		if (n.computed &&   // Filter for only member expressions using bracket notation
-			// Ignore member expressions with properties which can't be non-computed, like arr[2] or window['!obj']
-			// or those having another variable reference as their property like window[varHoldingFuncName]
-			(n.type === 'MemberExpression' &&
+		if (n.computed &&   // Filter for only nodes using bracket notation
+							// Ignore nodes with properties which can't be non-computed, like arr[2] or window['!obj']
+							// or those having another variable reference as their property like window[varHoldingFuncName]
+			(((n.type === 'MemberExpression' &&
 				n.property.type === 'Literal' &&
 				validIdentifierBeginning.test(n.property.value) &&
 				!badIdentifierCharsRegex.test(n.property.value)) ||
@@ -34,23 +35,55 @@ function normalizeComputed(arb, candidateFilter = () => true) {
 			 *    ['miao']: 4     // Will be changed to 'miao: 4'
 			 *  };
 			 */
-			(['MethodDefinition', 'Property'].includes(n.type) &&
+			(relevantTypes.includes(n.type) &&
 				n.key.type === 'Literal' &&
 				validIdentifierBeginning.test(n.key.value) &&
-				!badIdentifierCharsRegex.test(n.key.value)) &&
-			candidateFilter(n)) {
-			const relevantProperty = n.type === 'MemberExpression' ? 'property' : 'key';
-			arb.markNode(n, {
-				...n,
-				computed: false,
-				[relevantProperty]: {
-					type: 'Identifier',
-					name: n[relevantProperty].value,
-				},
-			});
+				!badIdentifierCharsRegex.test(n.key.value))) &&
+			candidateFilter(n))) {
+			matchingNodes.push(n);
 		}
+	}
+	return matchingNodes;
+}
+
+/**
+ * Transform a computed property access node to use dot notation.
+ * @param {Arborist} arb
+ * @param {Object} n The AST node to transform
+ */
+export function normalizeComputedTransform(arb, n) {
+	const relevantProperty = n.type === 'MemberExpression' ? 'property' : 'key';
+	arb.markNode(n, {
+		...n,
+		computed: false,
+		[relevantProperty]: {
+			type: 'Identifier',
+			name: n[relevantProperty].value,
+		},
+	});
+}
+
+/**
+ * Convert computed property access to dot notation where the property is a valid identifier.
+ * This normalizes bracket notation to more readable dot notation.
+ * 
+ * Transforms:
+ *   console['log'] -> console.log
+ *   obj['methodName']() -> obj.methodName()
+ *   {['propName']: value} -> {propName: value}
+ * 
+ * Only applies to string literals that form valid JavaScript identifiers
+ * (start with letter/$/_, contain only alphanumeric/_/$ characters).
+ * 
+ * @param {Arborist} arb
+ * @param {Function} candidateFilter (optional) a filter to apply on the candidates list
+ * @return {Arborist}
+ */
+export default function normalizeComputed(arb, candidateFilter = () => true) {
+	const matchingNodes = normalizeComputedMatch(arb, candidateFilter);
+	
+	for (let i = 0; i < matchingNodes.length; i++) {
+		normalizeComputedTransform(arb, matchingNodes[i]);
 	}
 	return arb;
 }
-
-export default normalizeComputed;
